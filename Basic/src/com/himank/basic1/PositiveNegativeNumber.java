5322246e1a97f7ff99962144f9ea7f9d643218e4
Himank <himankkumawat11@gmail.com>
2022-07-01 23:23:16 +0530

    pgm to check number is +ve or -ve

---
+++ Basic/src/com/himank/basic1/PositiveNegativeNumber.java
@@ -0,0 +1,16 @@
+package com.himank.basic1;
+
+import java.util.Scanner;
+
+public class PositiveNegativeNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int no = sc.nextInt();
+        if (no >= 0)
+            System.out.println("Number is a positive number.");
+        else
+            System.out.println("Number is a negative number.");
+    }
+}
